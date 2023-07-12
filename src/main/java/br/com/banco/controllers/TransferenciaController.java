1f73518da448340c61d7f17dc8a07797ba7c79e3
guilherme <guilherme.santos@dellead.com>
2023-07-12 13:26:09 -0300

    created repositories, services and controllers structure for entities

---
+++ src/main/java/br/com/banco/controllers/TransferenciaController.java
@@ -0,0 +1,9 @@
+package br.com.banco.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transferencias")
+public class TransferenciaController {
+}

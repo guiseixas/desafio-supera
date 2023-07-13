4d7dade63ba5cb312578da3eb7da547e97619161
guilherme <guilherme.santos@dellead.com>
2023-07-13 15:39:11 -0300

    added cross origin annotation in transferencia controller

--- src/main/java/br/com/banco/controllers/TransferenciaController.java
+++ src/main/java/br/com/banco/controllers/TransferenciaController.java
@@ -4,6 +4,7 @@ import br.com.banco.entities.Transferencia;
 import br.com.banco.services.TransferenciaService;
 import br.com.banco.types.TransferenciaFilterType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class TransferenciaController {
     @Autowired
     private TransferenciaService transferenciaService;
 
+    @CrossOrigin
     @GetMapping
     public List<Transferencia> findAll(TransferenciaFilterType filter){
         return transferenciaService.findAll(filter);
